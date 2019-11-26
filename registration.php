b1a2656299a541eeb8f20c3f16dc5061c2aa7455
sinhasinhai <snehasinha348@gmail.com>
2019-11-26 16:28:34 +0530

    cancer_application(php)

---
+++ registration.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+require('db.php');
+// If form submitted, insert values into the database.
+if (isset($_REQUEST['email'])){ 
+ $email = stripslashes($_REQUEST['email']);
+ $email = mysqli_real_escape_string($con,$email);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($con,$password);
+ $trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (password, email, trn_date)
+VALUES ('$password', '$email', '$trn_date')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+</body>
+</html>
